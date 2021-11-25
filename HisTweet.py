21f78f002b8c96326c0f7d77e1e05db1e4a65dd2
NUbrian <brian.chan@negawatt.co>
2021-11-25 17:02:41 +0800

    2021-11-25

--- HisTweet.py
+++ HisTweet.py
@@ -4,32 +4,35 @@ import pytz
 import pandas as pd
 import snscrape.modules.twitter as sntwitter
 
-c = calendar.Calendar(firstweekday=calendar.SUNDAY)
+def GetSundayDate(month,nth_week):
+    year = datetime.today().year
+    c = calendar.Calendar(firstweekday=calendar.SUNDAY)
+    my_calendar = c.monthdatescalendar(year, month)
+    return [day for week in my_calendar for day in week if day.weekday() == calendar.SUNDAY and day.month == month][nth_week-1]
 
-year = datetime.today().year
-march = 3
-november = 11
+summer_start_date = GetSundayDate(3, 2)
+summer_end_date = GetSundayDate(11, 1)
 
-mar_calendar = c.monthdatescalendar(year,march)
-summer_start_date = [day for week in mar_calendar for day in week if day.weekday() == calendar.SUNDAY and day.month == march][1]
+timezone = pytz.timezone('UTC')
+summer_start_time = timezone.localize(time(13,30,0))
+summer_end_time = timezone.localize(time(20,0,0))
+winter_start_time = timezone.localize(time(14,30,0))
+winter_end_time = timezone.localize(time(21,0,0))
 
-nov_calendar = c.monthdatescalendar(year,november)
-summer_end_date = [day for week in nov_calendar for day in week if day.weekday() == calendar.SUNDAY and day.month == november][0]
+winter_start_time - timedelta(hours=2)
 
 today_date = datetime.today().strftime('%Y-%m-%d')
 next_date = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
 
-timezone = pytz.timezone('UTC')
-print(timezone.localize(time(9,0,0)))
-
-
-#stock_start_time = {'summer': '2021, 'winter' }
-
 tweets_list = []
 
 for tweet in sntwitter.TwitterSearchScraper(f'$TSLA since:{today_date} until:{next_date}').get_items():
-    if i > 100:
-        break
+    if summer_start_date < datetime.today().date() < summer_end_date:
+        if tweet.date < summer_start_time - timedelta(hours=2):
+            break
+    else:
+        if tweet.date < winter_start_time - timedelta(hours=2):
+            break
     tweets_list.append([tweet.date, tweet.id, tweet.content])
 
 tweets_df2 = pd.DataFrame(tweets_list, columns=['Datetime', 'Tweet Id', 'Text'])
